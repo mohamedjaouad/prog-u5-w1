56ae51ceac69d05ee2de530ea15a35dcda6254a6
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-07-03 09:41:44 +0200

    Initial commit

---
+++ src/main/java/mohamedjaouad/prog_u5_w1/ProgU5W1Application.java
@@ -0,0 +1,13 @@
+package mohamedjaouad.prog_u5_w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgU5W1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgU5W1Application.class, args);
+	}
+
+}
---
+++ src/test/java/mohamedjaouad/prog_u5_w1/ProgU5W1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mohamedjaouad.prog_u5_w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgU5W1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
